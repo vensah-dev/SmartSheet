d0260809769afeeeb469cb9eca5e8f4c4c60c27f
Vensah <83892875+vensah-dev@users.noreply.github.com>
2023-11-18 15:16:08 +0800

    Initial Commit

---
+++ SwiftAcceleratorProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftAcceleratorProject
+//
+//  Created by Venkatesh Devendran on 18/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftAcceleratorProject/SwiftAcceleratorProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAcceleratorProjectApp.swift
+//  SwiftAcceleratorProject
+//
+//  Created by Venkatesh Devendran on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAcceleratorProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
