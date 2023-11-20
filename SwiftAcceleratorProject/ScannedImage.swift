081a62d2286d76b28023932ee57fdb1ce6ce33c5
Vensah <83892875+vensah-dev@users.noreply.github.com>
2023-11-20 18:32:58 +0800

    yea bro

---
+++ SwiftAcceleratorProject/ScannedImage.swift
@@ -0,0 +1,16 @@
+//
+//  ScannedImages.swift
+//  SwiftAcceleratorProject
+//
+//  Created by Venkatesh Devendran on 20/11/2023.
+//
+
+public struct ScannedImage{
+    var id = UUID()
+    
+    var title: String
+    var caption: String
+    var image: UIImage
+
+    
+}
