350da270f19541681e56b922ad68b2d9f408728f
Vensah <83892875+vensah-dev@users.noreply.github.com>
2023-11-20 13:55:13 +0800

    fniefieifbe

---
+++ Main.swift
@@ -0,0 +1,18 @@
+//
+//  Main.swift
+//  SwiftAcceleratorProject
+//
+//  Created by Venkatesh Devendran on 20/11/2023.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Main()
+}
