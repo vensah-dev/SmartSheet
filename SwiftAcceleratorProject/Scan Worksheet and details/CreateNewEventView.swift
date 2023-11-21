1c18fdf3c9f91154c07dcfe88bdddf2d11613495
Vensah <83892875+vensah-dev@users.noreply.github.com>
2023-11-21 14:17:16 +0800

    fcssc

---
+++ SwiftAcceleratorProject/Scan Worksheet and details/CreateNewEventView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateNewEventView.swift
+//  SwiftAcceleratorProject
+//
+//  Created by Venkatesh Devendran on 21/11/2023.
+//
+
+import SwiftUI
+
+struct CreateNewEventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateNewEventView()
+}
