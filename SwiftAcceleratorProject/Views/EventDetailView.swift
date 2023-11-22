e5f1cae74b8b7922fed4fc619f61ff4f855ace65
Vensah <83892875+vensah-dev@users.noreply.github.com>
2023-11-22 12:45:48 +0800

    wddwdw

---
+++ SwiftAcceleratorProject/Views/EventDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  EventDetailView.swift
+//  SwiftAcceleratorProject
+//
+//  Created by Venkatesh Devendran on 22/11/2023.
+//
+
+import SwiftUI
+
+struct EventDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventDetailView()
+}
