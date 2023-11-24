0323cbdada2689fd034e7aab6d33840a6af26791
M. Allen Jeffrey <mpjallenjeffrey@gmail.com>
2023-11-24 14:17:32 +0800

    no disappear anymore

--- SwiftAcceleratorProject/Views/FilesViews/FilesView.swift
+++ SwiftAcceleratorProject/Views/FilesViews/FilesView.swift
@@ -1,14 +1,19 @@
 import SwiftUI
-import VisionKit
 
 struct FilesView: View {
     @StateObject private var dataManager = DataManager()
     @State private var selectedSubjectIndex = 0
-    
+
     var body: some View {
         NavigationView {
             VStack {
-                if !dataManager.scannedImages.isEmpty {
+                if dataManager.scannedImages.isEmpty {
+                    Text("No resources yet")
+                        .foregroundColor(.secondary)
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                } else {
                     List {
                         ForEach(dataManager.scannedImages.indices, id: \.self) { index in
                             // Filter the images based on the selected subject
@@ -34,7 +39,7 @@ struct FilesView: View {
                                             .aspectRatio(contentMode: .fill)
                                             .frame(width: 60, height: 60)
                                             .cornerRadius(5)
-                                        
+
                                         VStack(alignment: .leading) {
                                             Text(dataManager.scannedImages[index].title)
                                             if !dataManager.scannedImages[index].caption.isEmpty {
@@ -48,54 +53,33 @@ struct FilesView: View {
                         }
                         .onDelete(perform: delete)
                     }
-                    .toolbar {
-                        ToolbarItem(placement: .navigationBarLeading) {
-                            EditButton()
-                        }
-                        
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            Picker("Subject", selection: $selectedSubjectIndex) {
-                                if dataManager.scannedImages.isEmpty {
-                                    // If no subjects, don't display any options
-                                } else {
-                                    Text("All").tag(-1)
-                                    ForEach(0..<dataManager.scannedImages.count, id: \.self) { index in
-                                        Text(dataManager.scannedImages[index].subject)
-                                            .tag(index)
-                                    }
-                                }
-                            }
-                            .pickerStyle(MenuPickerStyle())
-                        }
-                        
-                        ToolbarItem(placement: .primaryAction) {
-                            NavigationLink(
-                                destination: WorksheetDetailView(scannedImages: $dataManager.scannedImages, dataManager: dataManager),
-                                label: {
-                                    Image(systemName: "plus.circle")
-                                }
-                            )
-                        }
-                    }
-                } else {
-                    Text("No resources yet")
-                        .font(.title3)
-                        .foregroundColor(.gray)
-                        .italic()
                 }
             }
-            .navigationTitle("Resources")
+            .navigationBarTitle("Resources")
+            .navigationBarItems(leading:
+                EditButton(),
+                trailing:
+                HStack {
+                    Picker("Subject", selection: $selectedSubjectIndex) {
+                        Text("All").tag(-1)
+                        ForEach(0..<dataManager.scannedImages.count, id: \.self) { index in
+                            Text(dataManager.scannedImages[index].subject)
+                                .tag(index)
+                        }
+                    }
+                    .pickerStyle(MenuPickerStyle())
+
+                    NavigationLink(
+                        destination: WorksheetDetailView(scannedImages: $dataManager.scannedImages, dataManager: dataManager),
+                        label: {
+                            Image(systemName: "plus.circle")
+                        }
+                    )
+                }
+            )
         }
     }
-    
-    var selectedSubjectLabel: String {
-        if selectedSubjectIndex == -1 {
-            return "All"
-        } else {
-            return dataManager.scannedImages[selectedSubjectIndex].subject
-        }
-    }
-    
+
     func delete(at offsets: IndexSet) {
         dataManager.scannedImages.remove(atOffsets: offsets)
         dataManager.saveScannedImages()
