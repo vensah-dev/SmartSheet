42c2054d4e4317b2d998a077eef49230f45853d0
Vensah <83892875+vensah-dev@users.noreply.github.com>
2023-11-25 20:10:55 +0800

    CALENDAR STAYS

---
+++ SwiftAcceleratorProject/Views/FilesViews/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerView.swift
+//  SwiftAcceleratorProject
+//
+//  Created by Venkatesh Devendran on 25/11/2023.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerView()
+}
