929599d78a5e1c2d6480f90f7f86a8386d1cdc3c
M. Allen Jeffrey <mpjallenjeffrey@gmail.com>
2023-11-26 14:13:17 +0800

    testing images

--- SwiftAcceleratorProject/Views/HomeView.swift
+++ SwiftAcceleratorProject/Views/HomeView.swift
@@ -8,102 +8,31 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State var Streak = 4
-    @State var NextStreak = 10
-    @State var BestStreak = 7
+    @State var streak = 4
+    @State var nextStreak = 10
+    @State var bestStreak = 7
     @State var StreakCompletion = 40.0
     @State var OrangeText: Color = Color(UIColor(red: 255/255, green: 161/255, blue: 20/255, alpha: 0.8))
-    
     @State var lightOrangeText: Color = Color(UIColor(red: 255/255, green: 242/255, blue: 242/255, alpha: 1))
     
     var body: some View {
-        NavigationStack{
-            List{
-                //Streaks Widget
-                Section{
-                    ZStack {
-                        Image("StreaksHome")
-                            .scaledToFit()
-                            .padding(10)
-                            .opacity(0.8)
-                        
-                        VStack{
-                            HStack{
-                                Text("""
-                                     Current
-                                     Streak
-                                     """)
-                                .foregroundStyle(lightOrangeText)
-                                .font(.system(size: 22, weight: .bold, design: .rounded))
-                                .padding(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
-                                
-                                Text(String(Streak))
-                                    .foregroundStyle(OrangeText)
-                                    .font(.system(size: 64, weight: .semibold, design: .rounded))
-                                    .padding(.init(top: 0, leading: 30, bottom: 0, trailing: 0))
-                            }
-                            .frame(maxWidth: .infinity, maxHeight: 170.4, alignment: .leading)
-                            .padding(.init(top: 360, leading: 0, bottom: 0, trailing: 0))
-                            
-                            
-                            VStack{
-                                
-                                ProgressView("", value: StreakCompletion, total: 100)
-                                    .tint(lightOrangeText)
-                                    .frame(width: 202)
-                                
-                                Text("\(NextStreak - Streak) days to go")
-                                    .opacity(0.47)
-                                    .font(.system(size: 12, weight: .medium, design: .rounded))
-                                    .padding(.init(top: 0, leading: 120, bottom: 0, trailing: 0))
-                                
-                            }
-                            .padding(.init(top: 0, leading: 0, bottom: 0, trailing: 133))
-                        }
-                        .padding(.init(top: 0, leading: 15, bottom: 360, trailing: 0))
-                        
-                        VStack{
-                            Text("BestStreak")
-                                .foregroundStyle(OrangeText)
-                                .font(.system(size: 22, weight: .bold, design: .rounded))
-                                .padding(.init(top: 0, leading: 0, bottom: 0, trailing: 3))
-                            
-                            Text(String(BestStreak))
-                                .foregroundStyle(OrangeText)
-                                .font(.system(size: 64, weight: .bold, design: .rounded))
-                                .padding(.init(top: 10, leading: 0, bottom: 0, trailing: 0))
-                            
-                        }
-                        .padding(.init(top: 0, leading: 224, bottom: 0, trailing: 0))
-                        
-                    }
-                    .padding(.init(top: -300, leading: 0, bottom: -300, trailing: 0))
-                }
-                .listRowBackground(Color.red.opacity(0.0))
+        GeometryReader { geometry in
+            ZStack {
+                Image("streaksNormal")
+                    .resizable()
+                    .scaledToFit()
                 
-                Section(header: Text("Description")
-                    .font(.system(size: 17 ,weight: .bold, design: .rounded))
-                    .textCase(nil)){
-                        
-                    Text("ihwnd")
-                }
-                .listRowBackground(Color("lightOrange"))
+                Text(String(streak))
+                    .font(.system(size: 72, weight: .bold))
+                    .position(x: ((geometry.size.width / 2) - 30), y: ((geometry.size.height / 2) - 20))
+                
+                Text(String(bestStreak))
+                    .font(.system(size: 102, weight: .bold))
+                    .position(x: ((geometry.size.width / 2) + 130), y: ((geometry.size.height / 2) + 10))
+                
             }
-            .navigationBarTitle("Welcome, Back!")
-            .listStyle(InsetGroupedListStyle())
-            .scrollContentBackground(.hidden)
-            
-            
-            
         }
-        .navigationTitle("Welcome Back!")
-        .frame(maxWidth: .infinity)
-        .edgesIgnoringSafeArea(.all)
-        .listStyle(GroupedListStyle())
-        .scrollContentBackground(.hidden)
-        .opacity(1)
     }
-    
 }
 
 #Preview {
